7800e4f2aa7767d8a4502f191c5fedb482d671f9
Kenia Pliego <utti222032@utvco.edu.mx>
2025-05-21 10:02:05 -0600

    Cambios en app.html y app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'MyGirlyApp';
   selection = { value: '' };
+  constructor(){
+    console.log('Nuevas funciones');
+    console.warn('Cuidado');
+    console.log('Prueba de conflictos')
+  }
 }
